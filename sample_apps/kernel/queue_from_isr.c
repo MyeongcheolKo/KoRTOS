6f7e605c9536a9c4904ed3e8f8051ec1798a18cb
krisko <myeongcheolko@gmail.com>
2026-09-10 01:42:15 -0400

    Remove dead FPU warning from kernel sample apps

--- sample_apps/kernel/queue_from_isr.c
+++ sample_apps/kernel/queue_from_isr.c
@@ -4,9 +4,6 @@
 #include "kortos.h"
 #include "GPIO.h"
 
-#if !defined(__SOFT_FP__) && defined(__ARM_FP)
-#warning "FPU is not initialized, but the project is compiling for an FPU. Please initialize the FPU before use."
-#endif
 
 /*
 queue_from_isr.c
--- sample_apps/kernel/round_robin_priority.c
+++ sample_apps/kernel/round_robin_priority.c
@@ -3,9 +3,6 @@
 
 #include "kortos.h"
 
-#if !defined(__SOFT_FP__) && defined(__ARM_FP)
-  #warning "FPU is not initialized, but the project is compiling for an FPU. Please initialize the FPU before use."
-#endif
 
 /*
 round_robin_priority.c
